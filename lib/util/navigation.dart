7c6bd3ac789bb87e064018441b0c921d217ff20f
Nico Mexis <nico.mexis@kabelmail.de>
2021-04-14 19:30:57 +0200

    Fix navigator

--- lib/util/navigation.dart
+++ lib/util/navigation.dart
@@ -2,7 +2,7 @@ import 'package:flutter/widgets.dart';
 
 void navigateTo(BuildContext context, String name) {
   final r = ModalRoute.of(context);
-  if (r == null || r.settings.name != '/') {
-    Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+  if (r == null || r.settings.name != name) {
+    Navigator.pushNamedAndRemoveUntil(context, name, (route) => false);
   }
 }
